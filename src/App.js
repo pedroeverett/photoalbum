0503c2724c7b3fd78d51c1e65a6e171acb925cd1
Pedro Everett <pedrojoseeverett@gmail.com>
2019-02-19 11:24:52 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React, { Component } from 'react';
+import {Route, withRouter} from 'react-router-dom';
+import Nav from './components/Nav';
+import ListPhoto from './components/ListPhoto';
+import CreatePhoto from './components/CreatePhoto';
+import './App.css';
 
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Route exact path='/' component={ListPhoto} />
+                <Route exact path='/create' component={CreatePhoto} />
+            </div>
+        );
+    }
+}
+
+export default withRouter(App);
